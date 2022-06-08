268c22ffebf6ce1aa2282d19107497953f71835a
franciowel <spina_francesco@blu.it>
2022-06-08 16:00:16 +0200

    first commit

---
+++ js-mail/js/script.js
@@ -0,0 +1,27 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// Consigli del giorno:
+// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+// 3. si ma noi cosa vogliamo fare?
+// 4. torniamo a scrivere in italiano
+// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro 
+// programma così come lo faremmo "a mano"
+
+// // START
+
+// DATA UNA LISTA DI MAIL
+const ServerMail = ['francesco@mail.it', 'lolletto@mail.it','armando@mail.it', 'biondina@mail.it']
+console.log(ServerMail)
+console.log(ServerMail[0])
+
+// CHIEDIAMO ALL'UTENTE LA SUA MAIL
+
+// UNA VOLTA LETTA LA SUA MAIL CONTROLLO NELLA LISTA DELLE MIE MAIL SE LA SUA VI E
+
+// UNA VOLTA CONTROLLATA LA LISTA:
+    // se non c'è : messaggio file not found
+    // se c'è : messaggio di conferma
